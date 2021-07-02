68d14c6c7e39d2a1419b89441d4e4b35059349a4
s7023369667 <s7023369667@gmail.com>
2021-07-02 15:16:58 +0800

    test

--- app.py
+++ app.py
@@ -131,12 +131,11 @@ def handle_message(event):  # 收到訊息時
             print(out)  # heroku上output
             
             make_img(rgb)
-            img_link = '34.238.108.61/color_fig/b{0}g{1}r{2}.jpg'.format(rgb[2], rgb[1], rgb[0])
+            img_link = './color_fig/b{0}g{1}r{2}.jpg'.format(rgb[2], rgb[1], rgb[0])
             message2 = TextSendMessage(text='success')
             #img_link = ngrok_https + '/b{0}g{1}r{2}.jpg'.format(rgb[2], rgb[1], rgb[0])
             message1 = ImageSendMessage(original_content_url=img_link, preview_image_url=img_link)
             # message1 = TextSendMessage(text="debugging")
-
             #message2 = TextSendMessage(text=out[0].decode('utf-8'))
             #p.stdin.close()
             line_bot_api.reply_message(event.reply_token, [message2,message1])
--- mqtt_pub.py
+++ mqtt_pub.py
@@ -7,7 +7,7 @@ IP = "34.238.108.61" #heroku fixie proxy IP address
 client = mqtt.Client()
 client.username_pw_set("iot", "server")
 client.connect(IP, 1883, 60)
-print("IP:34.238.108.61")
+print(f"IP:{IP}")
 line = [str(n) for n in input().split()]
 mydict = {"userID": line[0], "R": line[1], "G": line[2], "B": line[3], "time": time.time()}
 client.publish("Line Server", "{userID} {R} {G} {B} {time}".format(**mydict))
