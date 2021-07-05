7ceaed34d7a3eba79421db6bf60fba70b7242ac9
s7023369667 <s7023369667@gmail.com>
2021-07-06 01:20:41 +0800

    last version

--- sensor.py
+++ sensor.py
@@ -1,14 +1,22 @@
 import serial
 import time
 import pymongo
+import paho.mqtt.client as mqtt
 ##sensor to mongodb
 
 def arduino_conect():
     BAUD = 9600
     #PORT = "COM5"  #windows
-    #PORT = "/dev/ttyACMB"
+    #PORT = "/dev/ttyACM8" #raspberry pi[-][_]"[-]"[_]
     PORT = '/dev/tty.usbmodem14201' #mac
     return serial.Serial(PORT, BAUD)
+
+def get_data(line):
+    line_list = line.split()
+    bottom = line_list[-1]
+    rgb = (int(line_list[1]), int(line_list[3]), int(line_list[5]))
+    print(rgb, bottom)
+    return rgb,bottom
 
 def main():
     #time.sleep(3)
@@ -18,29 +26,25 @@ def main():
     while True:
         time.sleep(0.1)
         line = arduino.readline().decode('utf-8')
-        *freq, rgb, bottom = line.split()
-        print(rgb, bottom)
+        rgb,bottom=get_data(line)
+        time.sleep(0.1)
         if bottom == '0':
+            print("Start initializing....")
             # initial
             for _ in range(50):
                 time.sleep(0.1)
                 line = arduino.readline().decode('utf-8')
                 print('init',_, line)
             
-            print('Start to pub.....')
+            print('Start to publish.....')
             for _ in range(30):  # 取得第三秒數據
                 time.sleep(0.1)
                 line = arduino.readline().decode('utf-8')
-            *freq, rgb, bottom = line.split()
-            r, g, b = [int(e) for e in rgb[1:-1].split(',')]
-            # for _ in range(9):
-            #     line = arduino.readline().decode('utf-8')
-            #     *temp, t_rgb, t_bottom = line.split()
-            #     t_r, t_g, t_b = [int(e) for e in t_rgb[1:-1].split(',')]
-            #     r, g, b = r+t_r, g+t_g, b+t_b
-            # r, g, b = r//10, g//10, b//10
+            rgb, bottom = get_data(line)
+            rgb=','.join([str(i) for i in rgb])
+            r, g, b = [int(e) for e in rgb[:].split(',')]
             data = {'R': r, 'G': g, 'B': b, 'time': time.asctime(time.localtime(time.time()))}
-            print('pub: r={R} g={G} b={B} time={time}'.format(**data))
+            print('publish: r={R} g={G} b={B} time={time}'.format(**data))
             time.sleep(5)
             # pub the data to server
             USERNAME = 's7023369667'
